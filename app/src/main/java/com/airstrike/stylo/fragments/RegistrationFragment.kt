3ccc7aa9714ac65d05705bdba696a3c819898c41
Filip1402 <zutifilip@gmail.com>
2023-11-27 19:31:56 +0100

    Implemented registration validation and showing/hiding password, missing connection to server. Validation is pretty basic. Created customer model. TODO: implement communication with server and send registration request to server

--- app/src/main/java/com/airstrike/stylo/fragments/RegistrationFragment.kt
+++ app/src/main/java/com/airstrike/stylo/fragments/RegistrationFragment.kt
@@ -1,14 +1,31 @@
 package com.airstrike.stylo.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.airstrike.stylo.R
+import com.airstrike.stylo.helpers.InputValidator
+import com.airstrike.stylo.models.Customer
 
 class RegistrationFragment : Fragment() {
 
+    private lateinit var etFirstName : EditText
+    private lateinit var etLastName : EditText
+    private lateinit var etEmail : EditText
+    private lateinit var etPhoneNum : EditText
+    private lateinit var etPassword : EditText
+    private lateinit var btnShowHidePassword : ImageButton
+    private lateinit var btnLoginRedirect : TextView
+    private lateinit var btnRegister : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -21,4 +38,63 @@ class RegistrationFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_registration, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        etFirstName = view.findViewById(R.id.first_name_et)
+        etLastName = view.findViewById(R.id.last_name_et)
+        etEmail = view.findViewById(R.id.email_et)
+        etPhoneNum = view.findViewById(R.id.phone_num_et)
+        etPassword = view.findViewById(R.id.password_et)
+        btnShowHidePassword = view.findViewById(R.id.hide_show_password_btn)
+        btnLoginRedirect = view.findViewById(R.id.sign_in_redirect_btn)
+        btnRegister = view.findViewById(R.id.register_btn)
+
+        btnShowHidePassword.setOnClickListener{
+            changePasswordDisplayMode()
+        }
+        btnLoginRedirect.setOnClickListener{
+            TODO("Implement login fragment redirect")
+        }
+        btnRegister.setOnClickListener{
+            if(checkIfDataIsValid()) {
+                var customer = getCustomerFromInput()
+                TODO("send registration request to server")
+            }
+        }
+    }
+    private fun getCustomerFromInput(): Customer {
+        return Customer(
+            etFirstName.text.toString(),
+            etLastName.text.toString(),
+            etEmail.text.toString(),
+            etPhoneNum.text.toString(),
+            etPassword.text.toString())
+    }
+    private fun changePasswordDisplayMode()
+    {
+        if(etPassword.transformationMethod == HideReturnsTransformationMethod.getInstance()) {
+            etPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+            btnShowHidePassword.setImageResource(R.drawable.hide_password)
+        }
+        else {
+            etPassword.transformationMethod = HideReturnsTransformationMethod.getInstance()
+            btnShowHidePassword.setImageResource(R.drawable.show_password)
+        }
+    }
+    private fun checkIfDataIsValid(): Boolean
+    {
+        val isEmailValid = InputValidator.validateEmail(etEmail.text.toString())
+        val isPhoneValid = InputValidator.validatePhoneNum(etPhoneNum.text.toString())
+        val isFirstNameValid = InputValidator.isNotEmpty(etFirstName.text.toString())
+        val isLastNameValid = InputValidator.isNotEmpty(etLastName.text.toString())
+        val isPasswordValid = InputValidator.isNotEmpty(etPassword.text.toString())
+        return if(isFirstNameValid && isLastNameValid && isEmailValid && isPhoneValid && isPasswordValid) {
+            Toast.makeText(context, "User input is valid!!!", Toast.LENGTH_LONG).show() //just for testing remove after
+            true
+        }else {
+            Toast.makeText(context, "User input is invalid!!!", Toast.LENGTH_LONG).show()//just for testing remove after
+            false
+        }
+    }
+
 }
+
---
+++ app/src/main/java/com/airstrike/stylo/helpers/InputValidator.kt
@@ -0,0 +1,19 @@
+package com.airstrike.stylo.helpers
+
+import android.util.Patterns
+
+object InputValidator {
+
+    fun validateEmail(email : String) : Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    fun validatePhoneNum(phoneNum: String): Boolean {
+        return Patterns.PHONE.matcher(phoneNum).matches() && phoneNum.length == 10 //keep in mind this isn't at all bullet proof
+        TODO("Implemt better check that supports international numbers and standards")
+    }
+    fun isNotEmpty(input: String): Boolean {
+        return input.isNotEmpty()
+    }
+
+}
