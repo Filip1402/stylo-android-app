7bdfa413207ff9695d70ca7b1ccdb83caf704295
Filip1402 <zutifilip@gmail.com>
2023-12-17 14:12:32 +0100

    Implemented redirect to homepage after successful login. Also e fragment is now a part of shopping activity

---
+++ app/src/main/java/com/airstrike/stylo/ShoppingActivity.kt
@@ -0,0 +1,19 @@
+package com.airstrike.stylo
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.airstrike.stylo.databinding.ActivityShoppingBinding
+
+class ShoppingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping)
+    }
+}
--- app/src/main/java/com/airstrike/stylo/fragments/HomepageFragment.kt
+++ app/src/main/java/com/airstrike/stylo/fragments/HomepageFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.airstrike.stylo.R
 
-class HompageFragment : Fragment() {
+class HomepageFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +18,7 @@ class HompageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hompage, container, false)
+        return inflater.inflate(R.layout.fragment_homepage, container, false)
     }
 
 }
