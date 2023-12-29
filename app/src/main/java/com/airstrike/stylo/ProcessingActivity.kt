5fab0e370b64b7abb587755104740a7f13b66ba9
fmilohano20 <fmilohano20@foi.hr>
2023-12-29 18:01:30 +0100

    added fap button and redirected to shopping processing activity

---
+++ app/src/main/java/com/airstrike/stylo/ProcessingActivity.kt
@@ -0,0 +1,11 @@
+package com.airstrike.stylo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProcessingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_processing)
+    }
+}
--- app/src/main/java/com/airstrike/stylo/ShoppingActivity.kt
+++ app/src/main/java/com/airstrike/stylo/ShoppingActivity.kt
@@ -1,5 +1,6 @@
 package com.airstrike.stylo
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -11,11 +12,19 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.airstrike.stylo.databinding.ActivityShoppingBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ShoppingActivity : AppCompatActivity() {
+
+    private lateinit var shoppingCartButton : FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
+        shoppingCartButton = findViewById(R.id.shoppingCartButton)
+        shoppingCartButton.setOnClickListener {
+            val intent = Intent(this, ProcessingActivity::class.java)
+            startActivity(intent)
+        }
     }
     fun loadFragment(newFragment : Fragment)
     {
@@ -24,5 +33,6 @@ class ShoppingActivity : AppCompatActivity() {
         transaction.replace(R.id.shopping_fragment_container,newFragment)
         transaction.commit()
     }
+
 
 }
