8c210fd603e05b8c550c2ea3d9cddd19e7b9315c
fmilohano20 <fmilohano20@foi.hr>
2023-12-29 15:12:58 +0100

    Improved back navigation on auth activity

--- app/src/main/java/com/airstrike/stylo/AuthenticationActivity.kt
+++ app/src/main/java/com/airstrike/stylo/AuthenticationActivity.kt
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.commit
 import com.airstrike.core.authentification.LoggedInUser
 import com.airstrike.core.authentification.helpers.JwtHandler
+import com.airstrike.stylo.fragments.ShoeDetails
 import com.airstrike.stylo.helpers.SecurePreferencesManager
 
 class AuthenticationActivity : AppCompatActivity() {
@@ -20,13 +23,30 @@ class AuthenticationActivity : AppCompatActivity() {
             startActivity(intent)
         }
         setContentView(R.layout.activity_authentication)
+
+        val callback = object : OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                val currentFragment = supportFragmentManager.findFragmentById(R.id.auth_fragment_container)
+                when(currentFragment)
+                {
+                    is ShoeDetails ->{
+                        supportFragmentManager.popBackStack()
+                    }
+                    else ->{
+                        isEnabled = false
+                        onBackPressedDispatcher.onBackPressed()
+                    }
+                }
+            }
+        }
+        onBackPressedDispatcher.addCallback(this,callback)
     }
     fun loadFragment(newFragment : Fragment)
     {
-        var fragmentManager : FragmentManager= supportFragmentManager
-        var transaction = fragmentManager.beginTransaction()
-        transaction.replace(R.id.auth_fragment_container,newFragment)
-        transaction.commit()
+        supportFragmentManager.commit {
+            replace(R.id.auth_fragment_container, newFragment)
+            addToBackStack(null)
+        }
     }
 
     private fun isUserAlreadyLoggedIn() : Boolean
